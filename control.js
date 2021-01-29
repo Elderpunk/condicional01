116ca2df46d590f9d1c4e0d96fb9820529b51333
jose <gonzaleztroyajc@gmail.com>
2021-01-29 10:55:36 +0100

    updated a switch

--- control.js
+++ control.js
@@ -1,5 +1,6 @@
 /**CONDICIONAL MULTIPLE */
 
+/**IF Y ELSE
 
 /** 
 
@@ -10,12 +11,21 @@
 
 * Y si, esta vacunada  se le dice que Pase por favor, esta autorizada para comprar la entrada */
 
-let credencial = prompt('¿Está usted vacunado?Por favor muestreme sus credenciales').toLowerCase()
+/**let credencial = prompt('¿Está usted vacunado?Por favor muestreme sus credenciales').toLowerCase()
 if(credencial === 'si') {
     console.log('Pase por favor, esta autorizada para comprar la entrada')
 }else if (credencial === 'no') {
     console.log('Lamentablemente debido al protocolo de salud de la comunidad Balear no permite que pueda comprar la entrada.Esperamos su rapida mejoria y esperamos verle nuevamente.')
+}*/
+/**SWITCH */
+let vacuna = prompt('¿Está usted vacunado?').toLowerCase()
+let persona = 'Jose'
+switch (vacuna) {
+    case 'si' : 
+    console.log(`${persona} Pase por favor, esta autorizada para comprar la entrada`)
+    break
+    case 'no' : 
+    console.log(`${persona} Lamentablemente debido al protocolo de salud de la comunidad Balear no permite que pueda comprar la entrada.Esperamos su rapida mejoria y esperamos verle nuevamente.`)
+    break
+    default: console.log(`${persona} conteste si o no`)
 }
-
-
-
